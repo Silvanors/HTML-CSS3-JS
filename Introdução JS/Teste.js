8186baf47c36fc0b9521935dfbf20b816f87456e
silvanors <silvanors@gmail.com>
2022-07-13 09:59:59 -0300

    introdução a js

---
+++ Introdução JS/Teste.js
@@ -0,0 +1,25 @@
+// var y = 0;
+var x=1;
+
+/* var y = 0;
+var x=1; 8*/
+
+//variáveis e constantes
+var preco = 2;
+var desconto = 0.2;
+const PRECO =2
+var total = PRECO - desconto;
+preco = preco - desconto
+
+//funcions
+
+function soma(a, b) {
+console.log(a+b);
+return a + b;
+}
+
+function soma(a, b) {
+    return a + b; // 8
+    }
+//chamar a função
+soma(3, 5);
