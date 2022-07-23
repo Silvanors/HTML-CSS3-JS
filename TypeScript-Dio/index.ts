cb531cedf1a9b8b372842ef2c5e48c0030860bb5
silvanors <silvanors@gmail.com>
2022-07-23 13:12:26 -0300

    aprendendo typescript

--- TypeScript-Dio/index.ts
+++ TypeScript-Dio/index.ts
@@ -1,1 +1,49 @@
-console.log('TypeScript');
+
+
+//types
+//interface
+
+/*interface IAnimal {
+    nome: string;
+    tipo:'terresetre' | 'aquático';
+    domestico: boolean;
+    
+}
+
+
+interface IFelino extends IAnimal {
+    visaoNoturna: boolean;
+}
+
+interface ICanino extends IAnimal {
+    porte: 'pequeno' | 'medio' |'grande';
+
+}
+
+type IDomestico = IFelino | ICanino;
+
+const animal: IAnimal = {
+    domestico: true,
+    nome: 'cachorro',
+    porte:'medio',
+    tipo: 'terresetre',
+    visaoNoturna: true,
+}
+
+
+
+const felino: IFelino = {
+    nome: 'leão',
+    tipo: 'terrestre',
+    visaoNoturna: true,
+}*/
+
+
+const input = document.getElementById('input') as HTMLInputElement;
+input.addEventListener('input', (event) => {
+const i = event.currentTarget as HTMLInputElement;
+console.log(i.value);
+
+//    console.log(event.currentTarget.)
+});
+    
